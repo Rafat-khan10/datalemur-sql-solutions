aa8e4737ff01bbe4ba642f2b116e5dc657dbbd22
Rafat-khan10 <rafatkhan2210@gmail.com>
2026-03-01 23:39:14 +0600

    Add solution : unfinished - parts

---
+++ easy/unfinished - parts.sql
@@ -0,0 +1,12 @@
+-- Problem: Unfinished Parts
+-- Difficulty: Easy
+-- Source: DataLemur
+-- Link: https://datalemur.com/questions/tesla-unfinished-parts
+
+-- Solution:
+
+SELECT 
+  part,
+  assembly_step
+FROM parts_assembly
+WHERE finish_date IS NULL
