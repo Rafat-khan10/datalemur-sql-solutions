7adf21e7696c2a9b3250fe1b726d616b19fcab6d
Rafat-khan10 <rafatkhan2210@gmail.com>
2026-03-02 23:56:37 +0600

    Add solution : laptop-vs-mobile-vewership

---
+++ easy/laptop-vs-mobile-vewership.sql
@@ -0,0 +1,17 @@
+-- Problem: Laptop vs. Mobile Viewership
+-- Difficulty: Easy
+-- Source: DataLemur
+-- Link: https://datalemur.com/questions/laptop-mobile-viewership
+
+-- Solution:
+
+SELECT SUM(CASE
+               WHEN device_type='laptop' THEN 1
+               ELSE 0
+           END) AS laptop_views,
+       SUM(CASE
+               WHEN device_type='tablet' THEN 1
+               WHEN device_type='phone' THEN 1
+               ELSE 0
+           END) AS mobile_views
+FROM viewership;
