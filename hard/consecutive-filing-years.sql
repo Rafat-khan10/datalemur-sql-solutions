57ca7400c72f0bdeaf3a60cbea63d3b3306de9a8
Rafat Khan <rafatkhan2210@gmail.com>
2026-07-10 01:47:25 +0600

    Add solution : consecutive-filing-years.sql

---
+++ hard/consecutive-filing-years.sql
@@ -0,0 +1,29 @@
+-- Problem: Consecutive Filing Years
+-- Difficulty: Hard
+-- Source: DataLemur
+-- Link: https://datalemur.com/questions/consecutive-filing-years
+
+-- Solution:
+
+WITH cte AS (SELECT
+  *
+FROM filed_taxes  
+WHERE product LIKE 'TurboTax%'
+), 
+
+cte2 AS (
+SELECT 
+   *,
+  LAG(filing_date) OVER(PARTITION BY user_id ORDER BY filing_date ASC) AS prev_filing_date,
+  filing_date AS current_filing_date,
+  LEAD(filing_date) OVER(PARTITION BY user_id ORDER BY filing_date ASC) AS next_filing_date
+FROM cte 
+)
+  
+SELECT
+  DISTINCT user_id
+FROM cte2 
+WHERE prev_filing_date IS NOT NULL
+AND next_filing_date IS NOT NULL 
+AND EXTRACT(YEAR FROM current_filing_date) - EXTRACT(YEAR FROM prev_filing_date) =1
+AND EXTRACT(YEAR FROM next_filing_date) - EXTRACT(YEAR FROM current_filing_date) =1
